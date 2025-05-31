b8ea0c89d481c724f120b07bf2ccb74d957dddb6
Zioo02 <kadzielat@gmail.com>
2025-05-31 22:33:53 +0200

    django

--- Django/djangotutorial/mysite/urls.py
+++ Django/djangotutorial/mysite/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from polls import views as polls_views 
+from mysite.views import home 
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('polls/', include('polls.urls')),
 ]
